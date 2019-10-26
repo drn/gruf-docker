a40cef2bbdbc0bd75229ac46e6798ef3ad1aa531
Darren Cheng <darren@thanx.com>
2019-10-26 12:22:56 -0700

    Add logs.

--- config/initializers/gruf.rb
+++ config/initializers/gruf.rb
@@ -1,5 +1,7 @@
+puts "Gruf Initializer: PRE"
 Gruf.configure do |c|
   c.server_binding_url = '0.0.0.0:50051'
   c.grpc_logger = Logger.new(STDOUT)
   c.logger = Logger.new(STDOUT)
 end
+puts "Gruf Initializer: POST"
